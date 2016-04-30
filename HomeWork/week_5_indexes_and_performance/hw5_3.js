348575d2461f87593dde9ad095cfa0464a3cae7a
ankurpatel18 <ankurp@glam.com>
2016-04-30 23:39:02 +0530

    updated HomeWork week 5 and week 6

--- HomeWork/week_5_indexes_and_performance/hw5_3.js
+++ HomeWork/week_5_indexes_and_performance/hw5_3.js
@@ -1,3 +1,3 @@
 mongoimport -d m101 -c profile < sysprofile.json
 
-db.profile.find({ns:/school2.students/}).sort({millis:-1}).pretty()
+db.profile.find({ns:/school2.students/}, {_id: 0, millis: 1 } ).sort( { millis: -1 } ).limit(3)
